0f25ee2386596e885407cbab1cbcc5274033a5d5
Oleksandr Chartoriiskiy <chartorijskij@gmail.com>
2023-08-26 20:04:38 +0300

    implemented ÐDECRYPT

--- src/main/java/ua/javarush/encoder/CaesarCipher.java
+++ src/main/java/ua/javarush/encoder/CaesarCipher.java
@@ -3,8 +3,8 @@ package ua.javarush.encoder;
 import java.nio.file.Path;
 
 public class CaesarCipher {
-    private char[] LETTERS_UPPERCASE;
-    private char[] LETTERS_LOWERCASE;
+    private final char[] LETTERS_UPPERCASE;
+    private final char[] LETTERS_LOWERCASE;
     private final int KEY;
 
     CaesarCipher(int key) {
@@ -14,7 +14,9 @@ public class CaesarCipher {
         LETTERS_LOWERCASE = language.ENGLISH_LOWERCASE;
     }
 
-    public Character encrypt(Path path, Character symbol) {
+    // TODO: 24.08.2023  створити окремий метод для розрахунку переповненого ключа (строка 27, 35)
+
+    public Character encrypt(Character symbol) {
         for (int i = 0; i < LETTERS_UPPERCASE.length; i++) {
 
             int indexOfNewSymbol = i + KEY;
@@ -22,7 +24,7 @@ public class CaesarCipher {
             if (LETTERS_UPPERCASE[i] == symbol) {
 
                 if (indexOfNewSymbol > LETTERS_UPPERCASE.length - 1) {
-                    indexOfNewSymbol -= LETTERS_UPPERCASE.length;
+                    indexOfNewSymbol = indexOfNewSymbol - ((indexOfNewSymbol / LETTERS_UPPERCASE.length) * LETTERS_UPPERCASE.length);
                 }
                 symbol = LETTERS_UPPERCASE[indexOfNewSymbol];
                 return symbol;
@@ -30,7 +32,7 @@ public class CaesarCipher {
             } else if (LETTERS_LOWERCASE[i] == symbol) {
 
                 if (indexOfNewSymbol > LETTERS_LOWERCASE.length - 1) {
-                    indexOfNewSymbol -= LETTERS_LOWERCASE.length;
+                    indexOfNewSymbol = indexOfNewSymbol - ((indexOfNewSymbol / LETTERS_LOWERCASE.length) * LETTERS_LOWERCASE.length);
                 }
                 symbol = LETTERS_LOWERCASE[indexOfNewSymbol];
                 return symbol;
@@ -40,5 +42,44 @@ public class CaesarCipher {
         return symbol;
     }
 
+    public Character decrypt(Character symbol) {
+        for (int i = 0; i < LETTERS_UPPERCASE.length; i++) {
+            if (LETTERS_UPPERCASE[i] == symbol) {
+                int indexNewChar = i - KEY;
 
+                if (indexNewChar < 0) {
+                    int sumOfCycles = KEY / LETTERS_UPPERCASE.length;
+
+                    indexNewChar = i - (KEY - (LETTERS_UPPERCASE.length * sumOfCycles));
+
+                    if (indexNewChar < 0) {
+                        indexNewChar = LETTERS_UPPERCASE.length - Math.abs(indexNewChar);
+                    }
+
+                }
+
+                symbol = LETTERS_UPPERCASE[indexNewChar];
+                return symbol;
+
+            } else if (LETTERS_LOWERCASE[i] == symbol) {
+                int indexNewChar = i - KEY;
+
+                if (indexNewChar < 0) {
+                    int sumOfCycles = KEY / LETTERS_LOWERCASE.length;
+
+                    indexNewChar = i - (KEY - (LETTERS_LOWERCASE.length * sumOfCycles));
+
+                    if (indexNewChar < 0) {
+                        indexNewChar = LETTERS_LOWERCASE.length - Math.abs(indexNewChar);
+                    }
+
+                }
+
+                symbol = LETTERS_LOWERCASE[indexNewChar];
+                return symbol;
+            }
+        }
+
+        return symbol;
+    }
 }
